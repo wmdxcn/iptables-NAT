a5ce887d093fffb6688e628b911fecdee279a57b
JasonXN <xnwx@foxmail.com>
2023-06-16 23:24:22 +0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -36,7 +36,7 @@ read -p "源服务器ip6(本地端口1006):" ip6
 read -p "源服务器ip7(本地端口1007):" ip7
 read -p "源服务器ip8(本地端口1008):" ip8
 read -p "源服务器ip9(本地端口1009):" ip9
-read -p "源服务器ip10(本地端口1001):" ip10
+read -p "源服务器ip10(本地端口1010):" ip10
 
 iptables -t nat -A PREROUTING -p tcp --dport 1001 -j DNAT --to-destination $ip1:1001
 iptables -t nat -A PREROUTING -p udp --dport 1001 -j DNAT --to-destination $ip1:1001
